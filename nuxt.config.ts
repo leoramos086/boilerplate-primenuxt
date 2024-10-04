8c7528e0c081e2d6fad657d00570c537fed5e71c
leandro.oliveira <leandro.oliveira@unyleya.edu.br>
2024-10-04 14:38:28 -0300

    start primevue theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+  primevue: {
+    importTheme: { from: '@/assets/theme.ts' },
+    options: {
+      inputVariant: 'filled',
+    },
+  },
   eslint: {
     config: {
       stylistic: true,
