a88f4106034678db35dad329daafeb210cf8ad65
leandro.oliveira <leandro.oliveira@unyleya.edu.br>
2024-10-04 12:24:03 -0300

    start tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
+export default <Config> {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
